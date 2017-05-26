cbedd0dfc4e9c25d2b8826e21168f7cf067e7e6b
Thomas Cheung <cheungth25@gmail.com>
2017-05-26 07:54:18 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,25 @@
 # Write your code here.
+def line(array)
+  if array.size == 0
+    return puts "The line is currently empty."
+  end
+  string = "The line is currently:"
+  array.each_with_index do |customer, spot_in_line|
+    string.concat(" #{spot_in_line+1}. #{customer}")
+  end
+  return puts string
+end
+
+def take_a_number(curr_line, person)
+  curr_line << person
+  return puts "Welcome, #{person}. You are number #{curr_line.length} in line."
+end
+
+def now_serving(curr_line)
+  if curr_line.length == 0
+    return puts "There is nobody waiting to be served!"
+  else
+    return puts "Currently serving #{curr_line.shift}."
+  end
+
+end
